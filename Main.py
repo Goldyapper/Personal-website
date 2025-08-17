2a0426dc03ec8d2f890edc5341b8316980cb8840
Adam Parsons <adam.a.parsons15@gmail.com>
2025-08-17 15:46:14 +0100

    aded a db committer for the rowing input data

--- Main.py
+++ Main.py
@@ -38,6 +38,15 @@ class Rowingdata(db.Model):
     # Relationship to Users
     user = db.relationship("Users", backref=db.backref("rowing_entries", lazy=True))
 
+    def __init__(self, user_id, leg_1, leg_2, leg_3, date=None):
+        self.user_id = user_id
+        self.leg_1 = leg_1
+        self.leg_2 = leg_2
+        self.leg_3 = leg_3
+        self.total = leg_1 + leg_2 + leg_3
+        if date:
+            self.date = date
+
 def extract_platform_number(name): # extract platform number
     match = re.search(r'\d+', name)
     return int(match.group()) if match else float('inf')
@@ -217,8 +226,12 @@ def rowing():
         leg_1 = float(request.form.get("leg_1",0))
         leg_2 = float(request.form.get("leg_2",0))
         leg_3 = float(request.form.get("leg_3",0))
-        total = leg_1 + leg_2 +leg_3
+        #total = leg_1 + leg_2 +leg_3
         print(total,todays_date)
+
+        current_entry = Rowingdata(current_user.id, leg_1, leg_2, leg_3)
+        db.session.add(current_entry)
+        db.session.commit()
 
         return render_template("rowing.html",leg_1=leg_1,leg_2=leg_2,leg_3=leg_3,total=total,date=todays_date)
 
